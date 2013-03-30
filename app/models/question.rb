a688cff5c1761ce6f10b2f5079bed05c2852ba26
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2013-03-30 18:09:24 +0000

    Questions are now submitted with the ID of the user who created it

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  attr_accessible :answer, :option_false, :option_true
-
+  attr_accessible :answer, :option_false, :option_true, :user_id
+  belongs_to :user
   has_many :answers
 end
